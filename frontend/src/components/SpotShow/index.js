d101818aa9c84e6c1b16ff0800e8c792e3a97218
Danny Yum <dhyum9@gmail.com>
2023-08-31 12:31:18 -0600

    CSS for SpotShow page

--- frontend/src/components/SpotShow/index.js
+++ frontend/src/components/SpotShow/index.js
@@ -3,6 +3,7 @@ import SpotShowImage from "../SpotShowImage";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { fetchSpotDetails } from "../../store/spot";
+import './SpotShow.css'
 
 const SpotShow = () => {
   const dispatch = useDispatch();
@@ -17,15 +18,47 @@ const SpotShow = () => {
     return null;
   }
 
+  const onClick = () => {
+    alert("Feature Coming Soon...");
+  };
+
  return (
   <div id='spot-details-container'>
     <h1>{spot.name}</h1>
     <div id='spot-details-second-row'>{spot.city}, {spot.state}, {spot.country}</div>
-    <div>
-      Hosted by {spot.Owner.firstName} {spot.Owner.lastName}
-    </div>
-    <div id="images-grid">
+    <div id="spot-details-third-row">
       <SpotShowImage images={spot.SpotImages}/>
+    </div>
+    <div id="spot-details-fourth-row">
+      <div id='fourth-row-left-col'>
+        <div id="host-information">
+          Hosted by {spot.Owner.firstName} {spot.Owner.lastName}
+        </div>
+        <div id='spot-information'>
+          {spot.description}
+        </div>
+      </div>
+      <div id='fourth-row-right-col'>
+        <div id='square-reserve-container'>
+          <div id='fourth-right-first'>
+            <div>${spot.price} <span style={{fontSize:"16px", display:"inline-block"}}> night</span></div>
+            <div id='review-info'>
+              <div style={{display:'flex', alignItems:'center', marginRight:"5px"}}>
+                <i class="fa-solid fa-star"></i>
+                <div>{spot.avgStarRating}</div>
+              </div>
+              <span>&#183;</span>
+              <div style={{textAlign: "center", marginLeft:"5px"}}>{spot.numReviews} reviews</div>
+            </div>
+          </div>
+          <div id='fourth-right-second'>
+            <button onClick={onClick}>Reserve</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr></hr>
+    <div id='review-section'>
     </div>
   </div>
  );
