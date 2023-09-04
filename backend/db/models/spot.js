3776f1c3fa5e304d101fca23c487510fcdcc9b46
Danny Yum <dhyum9@gmail.com>
2023-09-03 18:49:27 -0600

    Changed seeder user first names and text types of reviews and spot desc

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -90,7 +90,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false
     },
     price: {
