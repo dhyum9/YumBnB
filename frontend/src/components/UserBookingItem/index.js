2658065c242f59b0ba9f1af97c960e3acebfcfea
Danny Yum <dhyum9@gmail.com>
2023-10-31 09:46:52 -0600

    finalize booking item CSS, add onClick to booking item img and spot name

--- frontend/src/components/UserBookingItem/index.js
+++ frontend/src/components/UserBookingItem/index.js
@@ -1,7 +1,10 @@
 import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
 import DeleteReviewModal from "../DeleteReviewModal";
+import './UserBookingItem.css';
+import { useHistory } from "react-router-dom";
 
 const UserBookingItem = ({booking}) => {
+  const history = useHistory();
 
   const convertDate = (date) => {
     const months = {'01': "January", '02': "February", '03': "March", '04': "April", '05': "May", '06': "June", '07': "July", '08': "August", '09': "September", '10': "October", '11': "November", '12': "December"};
@@ -12,12 +15,18 @@ const UserBookingItem = ({booking}) => {
     return `${dateMonth} ${dateDays}, ${dateYear}`;
   }
 
+  const toSpotDetails = () => {
+    history.push(`/spots/${booking.spotId}`)
+  }
+
   return (
-    <div id='booking-item' style={{margin:"20px 0"}}>
-      <img src={booking.Spot.previewImage}></img>
-      <div>{booking.Spot.name}</div>
-      <div style={{color:'grey'}}>{convertDate(booking.startDate)} - {convertDate(booking.endDate)}</div>
-      {/* <div style={{fontSize:"14px", overflowWrap:"break-word"}}>{review.review}</div> */}
+    <div id='user-booking-item' style={{margin:"20px 0"}}>
+      <img className='user-booking-item-img' src={booking.Spot.previewImage} onClick={toSpotDetails}></img>
+      <div className='user-booking-item-info'>
+        <div className='user-booking-item-spot-name' onClick={toSpotDetails}>{booking.Spot.name}</div>
+        <div className='user-booking-item-owner'>Hosted by {booking.Spot.Owner.firstName} {booking.Spot.Owner.lastName}</div>
+        <div className='user-booking-item-date'>{convertDate(booking.startDate)} - {convertDate(booking.endDate)}</div>
+      </div>
       {/* <div className='fourth-row' style={{display:'flex', justifyContent:'start', margin: '10px -5px'}}>
           <button onClick={onClick} id='update-button'>
             Update
